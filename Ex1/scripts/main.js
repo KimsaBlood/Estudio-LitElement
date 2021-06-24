a1de08c6b59c6cc00d96294e0e89ee21c70d34e5
Diana Leticia Duran Ortiz <diana.duran@softtekprojects.com>
2021-06-23 21:14:19 -0500

    scripts3

--- Ex1/scripts/main.js
+++ Ex1/scripts/main.js
@@ -11,7 +11,9 @@ miImage.onclick = function () {
     }
 }
 
-let miBoton = document.querySelector('button');
+let miBoton = document.querySelector('button.my-btn');
+let nextBtn = document.querySelector('button.btn-next');
+let prevBtn = document.querySelector('button.btn-prev');
 miTitulo = document.querySelector( 'h1');
 
 function estableceNombreUsuario() {
@@ -32,6 +34,58 @@ else {
     miTitulo.textContent = 'Bievenid@ ' + nombreAlmacenado;
 }
 
+let images=["paramore","paramore2","paramore3","paramore4","paramore5"];
+
+function carousel(){
+    let i=0;
+    let aux=0;
+    setInterval(function(){
+        for(i=0;i<images.length;i++){
+            if(miImage.getAttribute("src").includes(images[i])){
+                aux=i+1;
+            }         
+        }   
+        if(aux===images.length){
+            aux=0;
+        } 
+        miImage.setAttribute("src","images/"+images[aux]+".jpg");
+    },10000);
+}
+
+function next(){
+    for(i=0;i<images.length;i++){
+        if(miImage.getAttribute("src").includes(images[i])){
+            aux=i+1;
+        }         
+    }   
+    if(aux===images.length){
+        aux=0;
+    } 
+    miImage.setAttribute("src","images/"+images[aux]+".jpg");
+}
+
+function prev(){
+    for(i=0;i<images.length;i++){
+        if(miImage.getAttribute("src").includes(images[i])){
+            aux=i-1;
+        }         
+    }   
+    if(aux<0){
+        aux=images.length-1;
+    } 
+    miImage.setAttribute("src","images/"+images[aux]+".jpg");
+}
+
+carousel();
+
 miBoton.onclick = function() {
     estableceNombreUsuario();
+}
+
+nextBtn.onclick = function() {
+    next()
+}
+
+prevBtn.onclick = function() {
+    prev();
 }
